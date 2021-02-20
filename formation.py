89ed5ef861ef507c6f3a0f8f6a648551070cdfbe
Kasra Alexander Sohrab <ksohrab3@gatech.edu>
2021-02-20 16:10:22 -0500

    self. and more comments

--- formation.py
+++ formation.py
@@ -6,17 +6,23 @@ class Formation:
     """Formation class"""
     def __init__(self):
         """Initializing the formation data"""
-        center = (field_width * .75, field_height * .75)
-        pygame.draw.circle(screen, GREEN, center, 1, 100)
+        self.player_list = []
+        self.center = (field_width * .75, field_height * .75)
 
     def create_formation(self, defenders_count, midfielders_count, attackers_count, strikers_count):
-        """Creates initial formation and positions that the robots will go to"""
+        """Creates initial formation and positions that the robots will go to
+
+        :param:
+        :param strikers_count: number of strikers in the formation
+        :param attackers_count: number of attackers in the formation
+        :param midfielders_count:  number of midfielders in the formation
+        :param defenders_count:  number of defenders in the formation
+        """
         # TODO: account for edge cases (where the inputs do not create a valid formation)
         # TODO: relative to center and ball (adjust width and depth)
-        player_list = []
         player_count = 0
         while player_count < 11:
-            player_list.append(FriendlyBot())
+            self.player_list.append(FriendlyBot())
             player_count += 1
 
         defense_width = field_width/(defenders_count + 1)
@@ -26,35 +32,48 @@ class Formation:
         # Set player initial positions where (0,0) is top left
 
         # Sweeper Keeper
-        player_list[0].update_position((field_width / 2, field_height - 20))
+        self.player_list[0].update_position((field_width / 2, field_height - 20))
 
         count = 1
         index = 1
         # Defenders
         while count <= defenders_count:
-            player_list[index].update_position((count * defense_width, 840))
+            self.player_list[index].update_position((count * defense_width, 840))
             count += 1
             index += 1
 
         # Midfielders
         count = 1
         while count <= midfielders_count:
-            player_list[index].update_position((count * midfield_width, 750))
+            self.player_list[index].update_position((count * midfield_width, 750))
             count += 1
             index += 1
 
         # Attackers
         count = 1
         while count <= attackers_count:
-            player_list[index].update_position((count * attack_width, 660))
+            self.player_list[index].update_position((count * attack_width, 660))
             count += 1
             index += 1
 
         # Strikers
         count = 1
         while count <= strikers_count:
-            player_list[index].update_position((count * strikers_width, 600))
+            self.player_list[index].update_position((count * strikers_width, 600))
             count += 1
             index += 1
 
-        return player_list
+        return self.player_list
+
+    def get_formation(self):
+        """formation getter method
+
+        :return: the list of players
+        """
+        return self.player_list
+
+    def move_center(self, position=(0, 0)):
+        """Moves the center and the bots move to keep in line with it.
+        """
+
+
