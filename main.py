a73065a4fc250681c2cfa4fcd8d33ac1edd15848
Kasra Alexander Sohrab <ksohrab3@gatech.edu>
2021-03-20 21:06:00 -0400

    ability to change formations

--- main.py
+++ main.py
@@ -15,13 +15,20 @@ def start_sim():
 
     pygame.init()
 
-    formation = Formation(11, field_width)
-    formation.set_positions(3, 4, 2, 1)
-    player_list = formation.create_formation()
+    # sample formation structures
+    formation3_4_2_1 = Formation(11, field_width)
+    formation3_4_2_1.set_positions(3, 4, 2, 1)
+
+    formation4_4_0_2 = Formation(11, field_width)
+    formation4_4_0_2.set_positions(4, 4, 0, 2)
+
+    # TODO: make formation iterable instead of using player_list
+    player_list = formation3_4_2_1.create_formation()
     running = True
     # Test move_center here
-    formation.move_center(player_list, (0, 0))
+    formation3_4_2_1.move_center(player_list, (0, 0))
 
+    formation_book = {0: formation3_4_2_1, 1: formation4_4_0_2}
     # stores the last clicked bot
     # we can update this when the user clicks on a bot
     # then the user can use arrow keys to move it
@@ -42,7 +49,6 @@ def start_sim():
                 for player in player_list:
                     dist_x = mouse_pos[0] - player.x
                     dist_y = mouse_pos[1] - player.y
-                    print(mouse_pos[0])
                     # Check if click is within bounds of a bot
                     if math.hypot(dist_x, dist_y) < player.radius:
                         print('collision at ' + str(player.x) + ', ' + str(player.y) + '. The click was ' +
@@ -50,8 +56,8 @@ def start_sim():
                         last_clicked_player = player
                         print(last_clicked_player)
 
-            # manual bot movement conditionals
             elif event.type == pygame.KEYDOWN:
+                # manual bot movement conditionals
                 # move clicked bot using arrow keys
                 if last_clicked_player is not None:
                     if event.key == pygame.K_LEFT:
@@ -63,6 +69,15 @@ def start_sim():
                     elif event.key == pygame.K_DOWN:
                         last_clicked_player.y += 3
 
+                # change formations mid-match
+                # if player clicks a number, choose that numbered formation from the list
+                elif 48 <= event.key <= 57:
+                    # TODO: change this?
+                    number = event.key - 48
+                    if formation_book.get(number) is not None:
+                        player_list = formation_book[number].create_formation()
+                    print(event.key)
+
             elif event.type == pygame.MOUSEBUTTONUP and event.button == RIGHT_CLICK:
                 # move clicked bot using right click on mouse
                 mouse_pos = pygame.mouse.get_pos()
@@ -71,7 +86,7 @@ def start_sim():
 
         screen.fill((0, 0, 0))
         # Formation center
-        pygame.draw.circle(screen, GREEN, formation.center, 3, 80)
+        pygame.draw.circle(screen, GREEN, formation3_4_2_1.center, 3, 80)
         # Halfway line
         pygame.draw.line(screen, WHITE, (0, field_depth / 2), (field_width, field_depth / 2))
         for player in player_list:
